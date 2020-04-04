649caae76e435508b0d57d5fdc0461a5a71392c0
dicksiano <dicksianomelo@gmail.com>
2020-04-04 20:09:47 -0300

    Full version of second problem

--- 2020/NestingDepth.cpp
+++ 2020/NestingDepth.cpp
@@ -2,29 +2,30 @@
 #include <bits/stdc++.h> 
 using namespace std; 
 
-string p(string s) {
-    string r = "";
-    int ones = false;
-    for(char c : s) {
-        if(c == '0') r += (ones ? ")" : "" ), ones = false;
-        else {
-            if(!ones) r += '(';
-            ones = true;
-        }
-
-        r += c;
+string p(string s, int i, int t, string &sol) {
+    if(i >= s.size()) {
+        while(t--) sol += ")";
+        return sol;
     }
-    return (ones) ? r + ')' : r;
+
+    int x = s[i] - '0';
+
+    for(; t > x; t--) sol += ")";
+    for(; t < x; t++) sol += "(";
+    sol.push_back(s[i]);
+
+    return p(s, i+1, t, sol);
 }
 
 int main() {
     int T;
-    string s;
+    string s, sol;
 
     cin >> T;
     for(int t = 0; t < T; t++) {
         cin >> s;
-        cout << "Case #" << t+1 << ": " << p(s) << endl;
+        sol = "";
+        cout << "Case #" << t+1 << ": " << p(s, 0, 0, sol) << endl;
     }
     return 0;
 }
